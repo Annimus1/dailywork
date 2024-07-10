0cb5357c47110c96dcb0962ebe13261cb6fb760b
pablo <pablo.j98@gmail.com>
2024-07-10 17:06:22 -0400

    create a view list of Jobs
    and also example data

---
+++ components/JobsList.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { View, Text, SafeAreaView, FlatList, StyleSheet, TouchableOpacity } from "react-native";
+import { Colors } from "@/constants/Colors";
+import testJobs from '@/util';
+
+export default function JobsList() {
+
+  return (
+    <SafeAreaView style={styles.list}>
+
+      <FlatList
+        data={testJobs}
+        renderItem={
+          ({ item }) => {
+            return (
+              <TouchableOpacity style={Item.container}>
+                <View style={Item.header}>
+                  <Text style={Item.title}>{item.title}</Text>
+                  <Text>${item.payment}</Text>
+                </View>
+                <View style={Item.header}>
+                  <Text>{item.date}</Text>
+                  <Text style={Item.category}>{item.category}</Text>
+                </View>
+              </TouchableOpacity>
+            );
+          }
+        }
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  list: {
+    width: 350,
+    height: 500,
+    marginBottom: 20,
+    borderRadius: 10,
+    paddingVertical: 4
+  },
+});
+
+const Item = StyleSheet.create({
+  container: {
+    padding: 5,
+    marginVertical: 4,
+    marginHorizontal: 5,
+    backgroundColor: Colors.light.background,
+    borderRadius: 4
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600'
+  },
+  category: {
+    fontSize: 14,
+    borderRadius: 9,
+    backgroundColor: Colors.dark.icon,
+    color: Colors.dark.text,
+    padding: 2
+  }
+});
---
+++ util.tsx
@@ -0,0 +1,155 @@
+import { Float } from "react-native/Libraries/Types/CodegenTypes";
+
+type Job = { 
+  date: string;
+  title: string;
+  description: string | null;
+  category: "Mantenimiento"|"Compresor"|"cambio de partes" | "carga de Gas";
+  payment: Float| null;
+  status : "Pagado" | "Pendiente"
+  paymentDue: string | null;
+}
+
+const testJobs: Job[] = [
+  {
+    date: '2023-07-09',
+    title: 'test1',
+    description: 'asmdlaksdml lakdnl lakdnml aksmdlmdla djqjbhduq m wdq skjq djhq j jq dq dygq su q sd qsd oqsd q',
+    category: 'Mantenimiento',
+    payment: 0.0,
+    status: 'Pendiente',
+    paymentDue: null
+  },
+  {
+    date: '2023-07-08',
+    title: 'test2',
+    description: 'Lorem Ipsum',
+    category: 'carga de Gas',
+    payment: 20.0,
+    status: 'Pagado',
+    paymentDue: '2023-07-08'
+  },
+  {
+    date: '2023-07-07',
+    title: 'test3',
+    description: null,
+    category: 'Compresor',
+    payment: 5.0,
+    status: 'Pagado',
+    paymentDue: '2023-07-07'
+  }, {
+    date: '2023-07-09',
+    title: 'test1',
+    description: 'asmdlaksdml lakdnl lakdnml aksmdlmdla djqjbhduq m wdq skjq djhq j jq dq dygq su q sd qsd oqsd q',
+    category: 'Mantenimiento',
+    payment: 0.0,
+    status: 'Pendiente',
+    paymentDue: null
+  }, {
+    date: '2023-07-09',
+    title: 'test1',
+    description: 'asmdlaksdml lakdnl lakdnml aksmdlmdla djqjbhduq m wdq skjq djhq j jq dq dygq su q sd qsd oqsd q',
+    category: 'Mantenimiento',
+    payment: 0.0,
+    status: 'Pendiente',
+    paymentDue: null
+  }, {
+    date: '2023-07-09',
+    title: 'test1',
+    description: 'asmdlaksdml lakdnl lakdnml aksmdlmdla djqjbhduq m wdq skjq djhq j jq dq dygq su q sd qsd oqsd q',
+    category: 'Mantenimiento',
+    payment: 0.0,
+    status: 'Pendiente',
+    paymentDue: null
+  }, {
+    date: '2023-07-09',
+    title: 'test1',
+    description: 'asmdlaksdml lakdnl lakdnml aksmdlmdla djqjbhduq m wdq skjq djhq j jq dq dygq su q sd qsd oqsd q',
+    category: 'Mantenimiento',
+    payment: 0.0,
+    status: 'Pendiente',
+    paymentDue: null
+  },{
+    date: '2023-07-07',
+    title: 'test3',
+    description: null,
+    category: 'Compresor',
+    payment: 5.0,
+    status: 'Pagado',
+    paymentDue: '2023-07-07'
+  },{
+    date: '2023-07-07',
+    title: 'test3',
+    description: null,
+    category: 'Compresor',
+    payment: 5.0,
+    status: 'Pagado',
+    paymentDue: '2023-07-07'
+  },{
+    date: '2023-07-07',
+    title: 'test3',
+    description: null,
+    category: 'Compresor',
+    payment: 5.0,
+    status: 'Pagado',
+    paymentDue: '2023-07-07'
+  },{
+    date: '2023-07-07',
+    title: 'test3',
+    description: null,
+    category: 'Compresor',
+    payment: 5.0,
+    status: 'Pagado',
+    paymentDue: '2023-07-07'
+  },{
+    date: '2023-07-07',
+    title: 'test3',
+    description: null,
+    category: 'Compresor',
+    payment: 5.0,
+    status: 'Pagado',
+    paymentDue: '2023-07-07'
+  },{
+    date: '2023-07-07',
+    title: 'test3',
+    description: null,
+    category: 'Compresor',
+    payment: 5.0,
+    status: 'Pagado',
+    paymentDue: '2023-07-07'
+  },{
+    date: '2023-07-07',
+    title: 'test3',
+    description: null,
+    category: 'Compresor',
+    payment: 5.0,
+    status: 'Pagado',
+    paymentDue: '2023-07-07'
+  },{
+    date: '2023-07-07',
+    title: 'test3',
+    description: null,
+    category: 'Compresor',
+    payment: 5.0,
+    status: 'Pagado',
+    paymentDue: '2023-07-07'
+  },{
+    date: '2023-07-07',
+    title: 'test3',
+    description: null,
+    category: 'Compresor',
+    payment: 5.0,
+    status: 'Pagado',
+    paymentDue: '2023-07-07'
+  },{
+    date: '2023-07-07',
+    title: 'test3',
+    description: null,
+    category: 'Compresor',
+    payment: 5.0,
+    status: 'Pagado',
+    paymentDue: '2023-07-07'
+  },
+]
+
+export default testJobs;
